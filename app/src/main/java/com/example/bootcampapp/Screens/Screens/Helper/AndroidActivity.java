108d640782aed4de8359965cbdd75662b8ef9bc2
Soham Kudalkar <sohamskudalkar02@gmail.com>
2024-02-09 12:53:21 +0530

    Followed bootcamp

--- app/src/main/java/com/example/bootcampapp/Screens/Screens/Helper/AndroidActivity.java
+++ app/src/main/java/com/example/bootcampapp/Screens/Screens/Helper/AndroidActivity.java
@@ -13,31 +13,21 @@ import com.example.bootcampapp.R;
 public class AndroidActivity extends AppCompatActivity {
 
 
-    Button btnOption1;
-    Button btnOption2;
-    Button btnOption3;
-
-    TextView questionText;
-
     // ARRAY OF QUESTIONS ============================================
-    private String[] questionsArray= {"Q.1 Which company developed android?", "Q.2 Which company bought android?", "Q.3 Web browser available in android is based on",
-            "Q.4 Android is based on which kernel?"};
+    private String[] questionsArray= {"Q.1 Which company developed android?", "Q.2 What is the current version of Android Studio?(as of Feb 2024)", "Q.3 Which is the official language of Android?", "Q.4 Who is the biggest competitor of android?"};
 
 
-    // ARRAY OF OPTION TEXT (INDEX - WISE) ============================================
-    private String[] btnOption1_text = {"Google", "Amazon", "Chrome", "Linux"};
-    private String[] btnOption2_text = {"Android", "Google", "Opera", "MAC"};
-    private String[] btnOption3_text = {"TCS", "HCL", "Open-source Webkit", "Hybrid"};
+    private String[] btnOption1Text = {"Google", "Flamingo", "Java", "Windows"};
+    private String[] btnOption2Text = {"Amazon", "Giraffe", "Swift","Linux" };
+    private String[] btnOption3Text = {"Jet Brains", "Hedgehog", "C++","IOS" };
+    private String[] btnOption4Text = {"Microsoft", "Monkey", "Kotlin", "Symbian"};
 
 
-    // ARRAY OF OPTION VALIDATION ============================================
-    private boolean[] validate1 = {false, false,false,true};
-    private boolean[] validate2 = {true, true,false,false};
-    private boolean[] validate3 = {false, false,true,false};
+    private boolean[] validateOption1 = {true, false, false, false};
+    private boolean[] validateOption2 = {false, false, false, false};
+    private boolean[] validateOption3 = {false, true, false, true};
+    private boolean[] validateOption4 = {false, false, true, false};
 
-
-
-    // INITIALISE THE VARIABLES ===============================================
     private int index = 0;
     private int score = 0;
 
@@ -46,75 +36,42 @@ public class AndroidActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android);
 
+        TextView question = findViewById(R.id.question);
+        Button button1 = findViewById(R.id.opt1);
+        Button button2 = findViewById(R.id.opt2);
+        Button button3 = findViewById(R.id.opt3);
+        Button button4 = findViewById(R.id.opt4);
 
+        question.setText(questionsArray[index]);
+        button1.setText(btnOption1Text[index]);
+        button2.setText(btnOption2Text[index]);
+        button3.setText(btnOption3Text[index]);
+        button4.setText(btnOption4Text[index]);
 
-
-        // REFER THE OPTION BUTTONS =====================================
-        btnOption1 = findViewById(R.id.opt1);
-        btnOption2 = findViewById(R.id.opt2);
-        btnOption3 = findViewById(R.id.opt3);
-
-
-
-
-
-        // REFER THE QUESTIONS TEXTVIEW & SET TEXT =====================================
-        questionText = findViewById(R.id.question);
-        questionText.setText(questionsArray[index]);
-
-
-
-
-        // REFER THE OPTION BUTTON & SET TEXT =====================================
-        btnOption1.setText(btnOption1_text[index]);
-        btnOption2.setText(btnOption2_text[index]);
-        btnOption3.setText(btnOption3_text[index]);
-
-
-
-
-
-
-        View.OnClickListener optionClickListener = new View.OnClickListener() {
+        View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(index <= questionsArray.length - 1) {
+                if (index < questionsArray.length) {
                     boolean[] currentValidation = null;
                     Button clickedButton = (Button) view;
 
-                    if (view == btnOption1) {
-                        currentValidation = validate1;
-                    } else if (view == btnOption2) {
-                        currentValidation = validate2;
-                    } else if (view == btnOption3) {
-                        currentValidation = validate3;
+                    if (view == button1) {
+                        currentValidation = validateOption1;
+                    } else if (view == button2) {
+                        currentValidation = validateOption2;
+                    } else if (view == button3) {
+                        currentValidation = validateOption3;
+                    } else if (view == button4) {
+                        currentValidation = validateOption4;
                     }
 
                     if (currentValidation != null) {
-                        if (currentValidation[index]) {
-                            score++;
-                        }
-                        index++;
-                        if (index <= questionsArray.length - 1) {
-                            questionText.setText(questionsArray[index]);
-                            btnOption1.setText(btnOption1_text[index]);
-                            btnOption2.setText(btnOption2_text[index]);
-                            btnOption3.setText(btnOption3_text[index]);
-                        } else {
-                            Toast.makeText(AndroidActivity.this, "Your score is: " + score + "/" + questionsArray.length, Toast.LENGTH_SHORT).show();
-                        }
+                        if (currentValidation[index])
+                                score++;
                     }
-                } else {
-                    Toast.makeText(AndroidActivity.this, "Restart the app", Toast.LENGTH_SHORT).show();
                 }
             }
         };
-
-
-        btnOption1.setOnClickListener(optionClickListener);
-        btnOption2.setOnClickListener(optionClickListener);
-        btnOption3.setOnClickListener(optionClickListener);
-
 
     }
 }
